6665010bca9e73ac65d7d9e83273bf9917684262
CristiAndLook <krhistian1902@gmail.com>
2024-01-04 22:30:37 -0500

    basic module distribution

--- fase-4-temas-avanzados/tema-11-modulos/leccion-02-paquetes/setup.py
+++ fase-4-temas-avanzados/tema-11-modulos/leccion-02-paquetes/setup.py
@@ -1,11 +1,13 @@
+from struct import pack
 from setuptools import setup
 
 setup(
-	name="paquete",
+	name="Mensajes",
 	version="0.1",
-	description="Este es un paquete de jemplo",
-	author="Hector Costa",
-	author_email="yo@hcosta.info",
-	url="http://hcosta.info",
-    packages=['paquete','paquete.hola','paquete.adios']
+	description="Este es un paquete de ejemplo",
+	author="LookAndCriss",
+	author_email="look@gmail.com",
+	url="http://lookandcriss.com",
+    packages=['paquete','paquete.hola','paquete.adios'],
+    scripts=['script.py']
 )
