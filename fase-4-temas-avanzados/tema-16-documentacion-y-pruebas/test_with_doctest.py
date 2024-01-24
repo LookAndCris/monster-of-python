e306b492b4802952dcd6a7008947b713acbc67a3
CristiAndLook <krhistian1902@gmail.com>
2024-01-23 22:31:26 -0500

    doctest script

---
+++ fase-4-temas-avanzados/tema-16-documentacion-y-pruebas/test_with_doctest.py
@@ -0,0 +1,14 @@
+def suma(a, b):
+    """Esta función recibe dos parámetros y devuelve la suma de ambos
+    
+    >>> suma(5,10)
+    15
+    """
+    return a+b
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    
+    
