883c549888b22d90eb166395fd9deffea9af23bb
CristiAndLook <krhistian1902@gmail.com>
2024-01-26 18:45:26 -0500

    Installer with pyinstaller

---
+++ installer/programa.py
@@ -0,0 +1,7 @@
+print('How create a installer with python?')
+
+# First step: install the library pyinstaller
+# pip install pyinstaller
+
+# Second step: Run the command
+# pyinstaller --onefile programa.py
